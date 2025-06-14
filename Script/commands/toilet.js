063b2e6804297807e80204eff0ae927b834aecc5
voice-station-bot2-0 <bablunayem9@gmail.com>
2025-06-14 20:54:31 +0600

    Update toilet.js

--- Script/commands/toilet.js
+++ Script/commands/toilet.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "toilet",
 	version: "1.0.1",
-	hasPermssion: 0,
+	hasPermssion: 1,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
 	description: "Toilet 🚽",
 	commandCategory: "Image",
